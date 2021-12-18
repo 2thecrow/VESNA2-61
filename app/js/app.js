36693040a8664ce8c56b9a1a00482f6cc56fcf51
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-12-18 19:21:31 +0200

    доделал слайдер с фильтрацией

--- app/js/app.js
+++ app/js/app.js
@@ -51,10 +51,23 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 
-	const swiper = new Swiper('.swiper', {
+	const flatsSlider = new Swiper('.flat-slider', {
 		// Optional parameters
 		autoHeight: true,
-        spaceBetween: 20,
+		spaceBetween: 30,
+		slidesPerView: 2,
+		watchSlidesProgress: true,
+
+		breakpoints: {
+			// when window width is >= 320px
+			375: {
+			  slidesPerView: 1,
+			},
+			// when window width is >= 480px
+			850: {
+			  slidesPerView: 2,
+			}
+		},
 		// If we need pagination
 		pagination: {
 			el: '.swiper-pagination',
@@ -67,6 +80,64 @@ document.addEventListener('DOMContentLoaded', () => {
 		  prevEl: '.swiper-button-prev',
 		},
 	  
+	});
+	const flatsNavSlider = new Swiper('.nav-slider', {
+		// Optional parameters
+		autoHeight: true,
+		spaceBetween: 30,
+		slidesPerView: 'auto',
+		freeMode: true,
+
+		observer: true,
+		observeParents: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+
+		breakpoints: {
+			// when window width is >= 320px
+			375: {
+				slidesPerView: 1,
+				freeMode: false,
+			},
+			// when window width is >= 480px
+			450: {
+				slidesPerView: 'auto',
+			}
+		},
+
+
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+			dynamicBullets: true,
+			clickable: true
+		},
+	  
+	});
+
+	/* Filter show flats */
+	$('.flats-nav__link').on('click', function () {
+		var filter = $(this).attr('data-filter');
+
+		$('.flat-slider .swiper-slide').css('display', 'none');
+		$('.flat-slider .swiper-slide.' + filter).css('display', 'block');
+
+		flatsSlider.updateSize();
+		flatsSlider.updateSlides();
+		flatsSlider.updateSlidesClasses();
+		flatsSlider.slideTo(0, 1000, false);
+		return false;
+	});
+	/* Show all flats */
+	$('.flats-show').on('click', function () {
+
+		$('.flat-slider .swiper-slide').css('display', 'block');
+
+		flatsSlider.updateSize();
+		flatsSlider.updateSlides();
+		flatsSlider.updateSlidesClasses();
+		flatsSlider.slideTo(0, 1000, false);
+		return false;
 	});
 
 })
